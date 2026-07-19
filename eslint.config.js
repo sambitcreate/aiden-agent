2f791245c831406167fe87bf9d7599ba48b1feac
Sambit Biswas <sambit.biswas2@gmail.com>
2026-07-18 20:02:18 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,25 @@
-import glazeConfig from "./node_modules/@glaze/core/cli/lint/eslint.config.js";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 
 export default [
   {
-    ignores: ["**/*.glaze/**"],
+    ignores: ["build/**", "release/**", "node_modules/**", ".memory/**", ".papercuts/**"],
   },
-  ...glazeConfig,
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaVersion: "latest", sourceType: "module", ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "no-undef": "off"
+    },
+  },
 ];
