519723b2a8ab4532e0a9d237866d9a48e5cbf609
Sambit Biswas <sambit.biswas2@gmail.com>
2026-08-12 00:56:07 -0400

    fix: preserve subagent IPC registration

--- main/handlers/index.ts
+++ main/handlers/index.ts
@@ -23,6 +23,7 @@ import { registerScheduledTaskHandlers } from "./scheduled-tasks.js";
 import { registerAssistantHandlers } from "./assistant.js";
 import { registerShortcutHandlers } from "./shortcuts.js";
 import { registerTelegramHandlers } from "./telegram.js";
+import { registerSubagentHandlers } from "./subagents.js";
 
 import { ipcMain, logger } from "../platform.js";
 import { writeDevLog } from "../services/dev-log.js";
@@ -63,6 +64,7 @@ export function registerHandlers(): void {
   registerAssistantHandlers();
   registerShortcutHandlers();
   registerTelegramHandlers();
+  registerSubagentHandlers();
 
   logger.info("handlers", "✓ IPC handlers registered");
 
